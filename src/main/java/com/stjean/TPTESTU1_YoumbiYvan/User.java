9da3ee80ad56c241c6579cce0ad105474b14c163
Youmbi-yvan4 <ngongang.yvan@institutsaintjean.org>
2024-10-25 11:12:09 +0100

    User and it's attribute

---
+++ src/main/java/com/stjean/TPTESTU1_YoumbiYvan/User.java
@@ -0,0 +1,89 @@
+package com.stjean.TPTESTU1_YoumbiYvan;
+
+import java.util.ArrayList;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String email;
+    private String phone;
+    private String city;
+    private double balancePersonnel;
+    public static ArrayList<User> users = new ArrayList<>();
+
+
+    public User(int id, String name, int age, String email, String phone, String city, double balancePersonnel) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.phone = phone;
+        this.city = city;
+        this.balancePersonnel = balancePersonnel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getBalancePersonnel() {
+        return balancePersonnel;
+    }
+
+    public void setBalancePersonnel(double balancePersonnel) {
+        this.balancePersonnel = balancePersonnel;
+    }
+
+    public static ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public static void setUsers(ArrayList<User> users) {
+        User.users = users;
+    }
+}
