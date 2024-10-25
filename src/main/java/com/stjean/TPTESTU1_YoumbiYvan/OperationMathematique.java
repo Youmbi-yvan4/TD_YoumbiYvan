dab27de853e9da5d3b9fc0db9018a94300bd9e5d
Youmbi-yvan4 <ngongang.yvan@institutsaintjean.org>
2024-10-25 10:55:39 +0100

    Add class and modify factorial method

--- src/main/java/com/stjean/TPTESTU1_YoumbiYvan/OperationMathematique.java
+++ src/main/java/com/stjean/TPTESTU1_YoumbiYvan/OperationMathematique.java
@@ -6,14 +6,16 @@ public class OperationMathematique {
     public boolean estPositif(int nombre) {
         return nombre > 0;
     }
-    public int factorial(int nombre) {
-        if (nombre < 0) throw new IllegalArgumentException("Negative number");
+
+    public int factorial(int nombre) throws IllegalParamISIException {
+        if (nombre < 0) throw new IllegalParamISIException("Negative number");
         int fact = 1;
         for (int i = 1; i <= nombre; i++) {
             fact *= i;
         }
         return fact;
     }
+
 
     public int[] sort(int[] lists) {
         Arrays.sort(lists);
