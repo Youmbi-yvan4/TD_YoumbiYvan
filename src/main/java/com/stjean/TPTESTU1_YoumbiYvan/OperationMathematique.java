e1abc7dcbab343339436a5080d59e63b203ea030
Youmbi-yvan4 <ngongang.yvan@institutsaintjean.org>
2024-10-25 09:27:29 +0100

    Add factorial method

--- src/main/java/com/stjean/TPTESTU1_YoumbiYvan/OperationMathematique.java
+++ src/main/java/com/stjean/TPTESTU1_YoumbiYvan/OperationMathematique.java
@@ -4,5 +4,13 @@ public class OperationMathematique {
     public boolean estPositif(int nombre) {
         return nombre > 0;
     }
+    public int factorial(int nombre) {
+        if (nombre < 0) throw new IllegalArgumentException("Negative number");
+        int fact = 1;
+        for (int i = 1; i <= nombre; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
 }
 
