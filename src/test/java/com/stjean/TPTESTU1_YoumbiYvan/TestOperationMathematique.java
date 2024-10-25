bbd20aab2ee115b4ccd0e8ef3370e7c46b72f7c4
Youmbi-yvan4 <ngongang.yvan@institutsaintjean.org>
2024-10-25 10:52:07 +0100

    sort test

--- src/test/java/com/stjean/TPTESTU1_YoumbiYvan/TestOperationMathematique.java
+++ src/test/java/com/stjean/TPTESTU1_YoumbiYvan/TestOperationMathematique.java
@@ -21,4 +21,13 @@ public class TestOperationMathematique {
         assertEquals(1, operation.factorial(0), "Factorial of 0 should be 1");
         assertEquals(1, operation.factorial(1), "Factorial of 1 should be 1");
         assertEquals(120, operation.factorial(5), "Factorial of 5 should be 120");
-    }}
+    }
+    @Test
+    public void testSort() {
+        OperationMathematique operation = new OperationMathematique();
+        int[] unsorted = {3, 1, 4, 1, 5, 9};
+        int[] sorted = {9, 5, 4, 3, 1, 1};
+        assertArrayEquals(sorted, operation.sort(unsorted), "Array should be sorted in descending order");
+    }
+
+}
