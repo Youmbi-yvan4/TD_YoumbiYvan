ee1e2e488168ac1c4da23676bca362d31acf4f00
Youmbi-yvan4 <ngongang.yvan@institutsaintjean.org>
2024-10-25 09:16:40 +0100

    First commit

---
+++ src/main/java/com/stjean/TPTESTU1_YoumbiYvan/Tptestu1YoumbiYvanApplication.java
@@ -0,0 +1,13 @@
+package com.stjean.TPTESTU1_YoumbiYvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tptestu1YoumbiYvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tptestu1YoumbiYvanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stjean/TPTESTU1_YoumbiYvan/Tptestu1YoumbiYvanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stjean.TPTESTU1_YoumbiYvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tptestu1YoumbiYvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
